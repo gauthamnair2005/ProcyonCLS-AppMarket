fcb95d65fa3b2c661fbd46563ad1fffe35c43ff3
Gautham Nair <gautham.nair.2005@outlook.com>
2024-12-06 16:47:02 +0530

    Update sample.py

--- apps/sample.py
+++ apps/sample.py
@@ -8,9 +8,9 @@ import ekernel
 
 def main():
     if len(sys.argv) == 3:
-        if sys.argv[1] == "0.9L":
+        if sys.argv[1] >= "1.5.0":
             if sys.argv[2] == "False":
-                ekernel.splashScreen("App Sample for ProcyonCLS", "Version 0.9L Compatible App 0.1")
+                ekernel.splashScreen("App Sample for ProcyonCLS", "Version 1.5.0 Compatible App 0.1")
                 ekernel.printHeader("Sample")
                 kernel.println("Hello, World!")
                 ekernel.prettyPrint("Hello, World!")
@@ -23,7 +23,7 @@ def main():
                 kernel.println("This is normal text")
             else:
                 # Admin Access True
-                ekernel.splashScreen("App Sample for ProcyonCLS", "Version 0.9L Compatible App 0.1")
+                ekernel.splashScreen("App Sample for ProcyonCLS", "Version 1.5.0 Compatible App 0.1")
                 ekernel.printHeader("Sample")
                 kernel.println("Hello, World!")
                 ekernel.prettyPrint("Hello, World!")
